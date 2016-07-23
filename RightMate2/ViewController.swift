3b0cd016359a3d15ad2f1a695e98bc721beeb5a5
FlyKnight <lakshaymaharana1@gmail.com>
2016-07-23 11:06:00 -0700

    Initial Commit

---
+++ RightMate2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RightMate2
+//
+//  Created by Riddhi Maharana on 7/23/16.
+//  Copyright © 2016 Lakshay Maharana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
